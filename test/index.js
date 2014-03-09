c33333e6a5dd4070da5ba96cfe80449d2b6da243
Elad Ben-Israel <elad.benisrael@gmail.com>
2014-03-09 11:27:25 +0200

    Remove redundant `m` in test name output

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var fs = require('fs');
 function requireFile(file) {
-  if (path.extname(file) === '.js'){
-    console.log("Tests for: \033[34mm%s\033[0m",  file);
+  if (path.extname(file) === '.js') {
+    console.log("Tests for: \033[34m%s\033[0m",  file);
     require("./" + file.replace('.js', ''));
   }
 }
